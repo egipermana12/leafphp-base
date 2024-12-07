1195fd91aea2bfadd50aa5baab238cd474cd829a
egipermana12 <egi.permana76@gmail.com>
2024-12-07 07:52:08 +0700

    tes

--- app/views/js/Components/ModalSimple.jsx
+++ app/views/js/Components/ModalSimple.jsx
@@ -18,16 +18,16 @@ const ModalSimple = ({ isOpen, onClose, title, children, acceptLabel, declineLab
         className="overflow-y-auto overflow-x-hidden fixed top-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full w-full"
       >
         <div className={`relative p-4 w-full max-h-full ${width}`}>
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="relative bg-white rounded-lg shadow">
             {/* Modal header */}
-            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
+            <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t">
+              <h3 className="text-xl font-semibold text-gray-900">
                 {title} {/* Menggunakan props title */}
               </h3>
               <button
                 type="button"
                 onClick={onClose} // Menutup modal saat tombol close diklik
-                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center "
               >
                 <span className="sr-only">Close modal</span>
               </button>
@@ -37,7 +37,7 @@ const ModalSimple = ({ isOpen, onClose, title, children, acceptLabel, declineLab
               {children} {/* Konten modal yang dinamis */}
             </div>
             {/* Modal footer */}
-            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+            <div className="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b">
               <button
                 onClick={onAccept} // Callback untuk tombol accept
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
